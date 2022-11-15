c5ba365c0bcb49cfa2a666310103a8d20878eb9f
smart@nic.be <smart@nic.be>
2022-11-15 17:04:54 +0000

    Basic Functionning SDN Controller

--- controller/config.sh
+++ controller/config.sh
@@ -2,8 +2,8 @@
 # Bridge name
 BRIDGE=ovsbr1
 # Link the bridge to the SDN controller
-sudo ovs-vsctl set-controller ovsbr1 tcp:0.0.0.0:6633
+sudo ovs-vsctl set-controller $BRIDGE tcp:0.0.0.0:6633
 # Enable OpenFlow
-sudo ovs-vsctl set bridge ovsbr1 protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13,OpenFlow14,OpenFlow15
+sudo ovs-vsctl set bridge $BRIDGE protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13,OpenFlow14,OpenFlow15
 # Set controller as out-of-band
-sudo ovs-vsctl set controller ovsbr1 connection-mode=out-of-band
+sudo ovs-vsctl set controller $BRIDGE connection-mode=out-of-band
--- controller/controller.py
+++ controller/controller.py
@@ -13,21 +13,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-An OpenFlow 1.0 L2 learning switch implementation.
-"""
+# Modified version of the simple_switch.py example from Ryu
 
-
+"""
+An OpenFlow 1.0 controller installing Per-connection flows.
+"""
 from ryu.base import app_manager
+from ryu.topology.event import EventSwitchEnter, EventSwitchLeave
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_0
 from ryu.lib.mac import haddr_to_bin
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
+from ryu.lib.packet import packet, ethernet, ether_types, arp, ipv4, tcp, udp
+from ryu.app.ofctl.api import get_datapath
 
+HOST_PORT = 2
+OUT_PORT = 1
+IP_TCP = 6
+IP_UDP = 17
+IP_ICMP = 1
 
 class SimpleSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
@@ -35,13 +40,78 @@ class SimpleSwitch(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
+        # Get a list of all the datapaths
+        # print("datapath: %s" % datapath)
+        # # Delete all existing flows
+        # self.delete_all_flows()
+        # # Init ARP flood
+        # self.arp_flood(datapath)
+        # # Init host egress
+        # self.host_egress(datapath)
 
-    def add_flow(self, datapath, in_port, dst, src, actions):
+    @set_ev_cls(EventSwitchEnter)
+    def onSwitchConnection(self, event):
+        self.datapaths = get_datapath(self)
+        self.delete_all_flows(self.datapaths[0])
+        self.arp_flood(self.datapaths[0])
+        self.host_egress(self.datapaths[0])
+
+    def delete_all_flows(self, datapath):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        mod = parser.OFPFlowMod(datapath=datapath, command=ofproto.OFPFC_DELETE,
+                                match=match)
+        datapath.send_msg(mod)
+
+    def arp_flood(self, datapath):
+        match = datapath.ofproto_parser.OFPMatch(dl_type=ether_types.ETH_TYPE_ARP)
+        actions = [datapath.ofproto_parser.OFPActionOutput(datapath.ofproto.OFPP_FLOOD)]
+        mod = datapath.ofproto_parser.OFPFlowMod(
+            datapath=datapath, 
+            match=match, 
+            cookie=0,
+            command=datapath.ofproto.OFPFC_ADD,
+            idle_timeout=0,
+            hard_timeout=0,
+            priority=0,
+            actions=actions)
+        datapath.send_msg(mod)
+
+    def host_egress(self, datapath):
+        match = datapath.ofproto_parser.OFPMatch(in_port=HOST_PORT)
+        actions = [datapath.ofproto_parser.OFPActionOutput(OUT_PORT)]
+        mod = datapath.ofproto_parser.OFPFlowMod(
+            datapath=datapath, 
+            match=match, 
+            cookie=0,
+            command=datapath.ofproto.OFPFC_ADD,
+            idle_timeout=0,
+            hard_timeout=0,
+            priority=0,
+            actions=actions)
+        datapath.send_msg(mod)
+
+    def add_flow(self, datapath, in_port, flow_spec, actions):
         ofproto = datapath.ofproto
 
-        match = datapath.ofproto_parser.OFPMatch(
-            in_port=in_port,
-            dl_dst=haddr_to_bin(dst), dl_src=haddr_to_bin(src))
+        if flow_spec["nw_proto"] == IP_ICMP:
+            match = datapath.ofproto_parser.OFPMatch(
+                in_port=in_port,
+                dl_type=ether_types.ETH_TYPE_IP,
+                nw_proto=flow_spec["nw_proto"],
+                nw_src=flow_spec["nw_src"])
+        elif flow_spec["nw_proto"] == IP_TCP or flow_spec["nw_proto"] == IP_UDP:
+            match = datapath.ofproto_parser.OFPMatch(
+                in_port=in_port,
+                # Specify that the packet is an IPv4 packet
+                dl_type=flow_spec['dl_type'],
+                nw_proto=flow_spec['nw_proto'],
+                nw_src=flow_spec['nw_src'],
+                tp_src=flow_spec['tp_src'])
+        else:
+            raise Exception("Unsupported nw_proto: %s" % flow_spec["nw_proto"])
 
         mod = datapath.ofproto_parser.OFPFlowMod(
             datapath=datapath, match=match, cookie=0,
@@ -54,44 +124,37 @@ class SimpleSwitch(app_manager.RyuApp):
     def _packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
-        ofproto = datapath.ofproto
-
+        flow_spec = {
+            'dl_type': '',
+            'nw_proto': '',
+            'nw_src': '',
+            'tp_src': ''
+        }
         pkt = packet.Packet(msg.data)
+        # Get the Ethernet frame
         eth = pkt.get_protocol(ethernet.ethernet)
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             # ignore lldp packet
             return
-        dst = eth.dst
-        src = eth.src
-
-        dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
-
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, msg.in_port)
-
-        # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = msg.in_port
-
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            out_port = ofproto.OFPP_FLOOD
-
-        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
-
-        # install a flow to avoid packet_in next time
-        if out_port != ofproto.OFPP_FLOOD:
-            self.add_flow(datapath, msg.in_port, dst, src, actions)
-
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
-
-        out = datapath.ofproto_parser.OFPPacketOut(
-            datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
-            actions=actions, data=data)
-        datapath.send_msg(out)
+        flow_spec['dl_type'] = eth.ethertype
+        # If the packet is an IPv4 packet
+        if (flow_spec['dl_type'] == ether_types.ETH_TYPE_IP):
+            # Get the IP packet
+            ip = pkt.get_protocol(ipv4.ipv4)
+            flow_spec['nw_proto'] = ip.proto
+            flow_spec['nw_src'] = ip.src
+            if (flow_spec['nw_proto'] == 6):
+                # Get the TCP packet
+                tcp_pkt = pkt.get_protocol(tcp.tcp)
+                flow_spec['tp_src'] = tcp_pkt.src_port
+            elif (flow_spec['nw_proto'] == 17):
+                # Get the UDP packet
+                udp_pkt = pkt.get_protocol(udp.udp)
+                flow_spec['tp_src'] = udp_pkt.src_port
+            actions = [datapath.ofproto_parser.OFPActionOutput(HOST_PORT)]
+            self.add_flow(datapath, msg.in_port, flow_spec, actions)
+            self.logger.info("packet received on port %s from %s", msg.in_port, ip.src)
 
     @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
     def _port_status_handler(self, ev):
