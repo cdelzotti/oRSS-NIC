f284df1bf674432b38546eadfcedd06322e16b8c
smart@nic.be <smart@nic.be>
2022-11-22 14:08:58 +0000

    Removing old files

--- tools/gen_skeletons.sh
+++
@@ -1,5 +0,0 @@
-rm src/bpf/xdp.skel.h
-clang -target bpf -g src/bpf/xdp.bpf.c -c -o src/bpf/xdp.o
-bpftool gen skeleton src/bpf/xdp.o > src/bpf/xdp.skel.h
-rm src/bpf/xdp.o
-echo "Skeleton generated in src/bpf/xdp.skel.h"
